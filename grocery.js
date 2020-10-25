26438796021542f80b3bc124cf41c941246ee740
Mike Colbert <mcolbert1@live.maryville.edu>
2020-10-25 12:40:41 -0500

    Final commit of week 1 assignment

---
+++ grocery.js
@@ -0,0 +1,21 @@
+var Grocery = /** @class */ (function () {
+    function Grocery(qty, item) {
+        this.purchased = false;
+        this.qty = qty;
+        this.item = item;
+        this.cost = 0;
+        console.log(this.item + ' added to my list');
+    }
+    return Grocery;
+}());
+var groceryArray = [];
+groceryArray.push(new Grocery(1, 'milk'));
+groceryArray.push(new Grocery(2, 'bread'));
+groceryArray.push(new Grocery(1, 'eggs'));
+for (var i = 0; i < groceryArray.length; i++) {
+    document.getElementById('grocery_list').innerHTML += "<p>"
+        + groceryArray[i].purchased + " &emsp; "
+        + groceryArray[i].qty + " &emsp; "
+        + groceryArray[i].item + " &emsp; "
+        + groceryArray[i].cost + "</p>";
+}
